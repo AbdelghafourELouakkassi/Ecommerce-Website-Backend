05d344828aea0434624386aa3af485a71c3a37f5
AbdelghafourELouakkassi <125968638+AbdelghafourELouakkassi@users.noreply.github.com>
2023-11-14 13:07:37 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,9 +3,23 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const stripe = require("stripe")(process.env.STRIPE_SK);
+const allowedOrigins = ['https://mystore-frontend-iota.vercel.app'];
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: function(origin, callback){
+    if (!origin) {
+      return callback(null, true);
+    }
+
+    if (allowedOrigins.includes(origin)) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+
+}));
 
 
 app.get('/',(req,res)=>{
